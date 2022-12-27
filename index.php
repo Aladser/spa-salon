6beb350b4cc90d71f1469a86c22c59cc7a5e8fb2
Aladser <aladser@gmail.com>
2022-12-27 13:07:08 +0900

    правка шапки сайта

--- index.php
+++ index.php
@@ -21,11 +21,19 @@
 <body>
 
     <footer class='footer'>
+        <!-- Вход/выход в личный кабинет -->
+        <?php
+            if($auth){
+                echo "<form class='form-auth' method='POST' action='../php_scriptes/exit.php'><input type='submit' class='btn-auth btn-exit' value='Выйти'> </form>";
+            }
+            else{
+                echo "<a class='btn-auth' href='../pages/login.php'>Войти</a>";
+            }
+        ?>
         <!--  отображение имени пользователя 
             не используется getCurrentUser(), чтобы не подключать db.php
-        -->
-        
-        <p class='user'><?php
+        -->        
+        <p class='user-footer'><?php
             if($auth){
                 $login = $_SESSION['login'];
                 $startHours = $_SESSION['startHours'];
@@ -36,30 +44,22 @@
                 echo $_SESSION['count']==1 ?  "Здравствуйте, Гость!" : "Хотите войти в личный кабинет?";       
             } 
         ?></p>
-        <!-- Выход из личного кабинета -->
+        <p class='name-company-footer'> СПА-салон <span class='name-company-footer__name'>На чиле</span></p>
+    </footer>
+
+    <main>
         <?php
-            if($auth){
-                echo "<form class='form-exit' method='POST' action='../php_scriptes/exit.php'><input type='submit' class='btn-auth' value='Выйти'> </form>";
-            }
-            else{
-                echo "<a class='btn-auth' href='../pages/login.php'>Войти</a>";
-            }
-        ?>
-        <!-- Отображение скидки -->
-        <p class='discount'><?php
             if($auth){
                 $time = explode(':', date('G:i'));
                 $nowHours = 23 - $time[0];
                 $nowMinutes = 60 - $time[1];
 
+                echo "<section class='container'><p class='discount-container'>";
                 echo "Для вас индивидуальное предложение! Спешите!  Осталось ";
                 echo $nowHours != 0 ? "$nowHours ч. $nowMinutes мин." : "$nowMinutes мин.";
+                echo "</p></section>";
             }
-        ?></p>
-    </footer>
-
-    <main>
-    Главная страница
+        ?>
     </main>
     
 </body>
