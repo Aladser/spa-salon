c782f26453228a086c33053eefc91362fa05e406
Aladser <aladser@gmail.com>
2023-01-02 21:40:13 +0900

    рефакторинг

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/modal.css">
     <link rel="stylesheet" href="css/birthdaySendWindow.css">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/loginWindow.css">
     <title> СПА-салон «На Чиле»</title>
 </head>
 
